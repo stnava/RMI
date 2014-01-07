647de692781d81e5ecde36e0f673a45adf270c79
stnava <stnava@gmail.com>
2014-01-07 10:36:38 -0500

    Update install_anstr_packages.sh
    simplifying install based on R-based package installation

--- stnava/install_anstr_packages.sh
+++ stnava/install_anstr_packages.sh
@@ -94,41 +94,24 @@ fi
 
 cd $RPDIR
 if [[ $INSTALLRPKG == 1 ]] ; then 
-pkgs=( fit.models_0.5-10.tar.gz robustbase_0.9-10.tar.gz mvtnorm_0.9-9993.tar.gz pcaPP_1.9-48.tar.gz rrcov_1.3-02.tar.gz robust_0.4-15.tar.gz
-boot_1.3-8.tar.gz	 timeDate_2160.97.tar.gz iterators_1.0.6.tar.gz foreach_1.4.0.tar.gz signal_0.7-2.tar.gz timeSeries_2160.95.tar.gz Rcpp_0.10.3.tar.gz mFilter_0.1-3.tar.gz doParallel_1.0.1.tar.gz abind_1.4-0.tar.gz magic_1.5-4.tar.gz stringr_0.6.2.tar.gz evaluate_0.4.3.tar.gz digest_0.6.3.tar.gz formatR_0.7.tar.gz markdown_0.5.4.tar.gz knitr_1.1.tar.gz pixmap_0.4-11.tar.gz rgl_0.93.928.tar.gz misc3d_0.8-4.tar.gz);
-names=( fit.models robustbase mvtnorm pcaPP rrcov robust boot timeDate iterators foreach signal timeSeries Rcpp mFilter doParallel abind magic stringr evaluate digest formatR markdown knitr pixmap rgl misc3d);
-######## now install R packages ##########
-counter=0
-for pkg in ${pkgs[*]} ; do
-  wget http://cran.r-project.org/src/contrib/$pkg
-  if [ ! -s $pkg ]; then
-    wget http://cran.r-project.org/src/contrib/Archive/${names[$counter]}/${pkg}
-  fi
-  echo install ${names[$counter]} to $RLDIR 
-  R CMD INSTALL -l $RLDIR $pkg 
-  let counter=counter+1
-done
+R --no-save <<RSCRIPT
+local({r <- getOption("repos"); 
+       r["CRAN"] <- "http://cran.r-project.org"; options(repos=r)})
+install.packages("Rcpp",type="source")
+mypkg<-c("signal","timeSeries","mFilter","MASS","robust","magic","knitr","pixmap","rgl","misc3d")
+for ( x in mypkg ) 
+  {
+  install.packages(x)
+  }
+RSCRIPT
 fi 
 
 
 ANTSRDIR=$RLDIR
 cd $ANTSRDIR
 echo install ANTsR to $ANTSRDIR 
-MYRLDIR=${ANTSRDIR}/ANTsR_src/ANTsR/src/ANTS/ANTS-build/lib
 RHLIB=` R RHOME`/lib
 echo R home lib is $RHLIB 
-# if [[ ${#R_LD_LIBRARY_PATH} -eq 0 ]] ; then 
-#  mybashstring=`echo export R_LD_LIBRARY_PATH=${MYRLDIR}:${RHLIB}:${R_LD_LIBRARY_PATH}:${LD_LIBRARY_PATH} `
-#  if [[ -s ~/.profile ]] ; then 
-#    echo $mybashstring >> ~/.profile
-#    source ~/.profile
-#  else 
-#    echo ${mybashstring} >> ~/.bash_profile
-#    source ~/.bash_profile
-#  fi 
-# else 
-#  echo "WARNING:  added $mybashstring to your environment R_LD_LIBRARY_PATH"
-# fi
 echo  check ${ANTSRDIR}/ANTsR_src/ANTsR
 if [[ ! -s ${ANTSRDIR}/ANTsR_src/ANTsR ]] ; then 
   mkdir ANTsR_src
@@ -141,6 +124,3 @@ echo call R CMD INSTALL now ...
 myrenvstring=`echo R_LIBS_USER=${ANTSRDIR}:${MYRLDIR}:\$\{R_LIBS_USER\} `
 echo ${myrenvstring} >> ~/.Renviron
 R CMD INSTALL -l $RLDIR  ANTsR
-
-
-
